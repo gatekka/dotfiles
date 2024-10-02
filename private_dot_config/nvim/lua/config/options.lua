79d89bf210ab1a64cc1e5136718d7672b1fb9726
chris <94351434+gatekka@users.noreply.github.com>
2024-10-02 11:25:43 -0700

    Update NVIM shiftwidth

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,4 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.shiftwidth = 8 -- Size of an indent
