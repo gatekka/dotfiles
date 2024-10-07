fb9d0ded4c57beffbd2bcefd25eb126c6985b322
chris <94351434+gatekka@users.noreply.github.com>
2024-10-07 14:32:27 -0700

    Remap moving lines up and down

--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -3,16 +3,16 @@
 -- Add any additional keymaps here
 
 -- Move lines in normal mode
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==")
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==")
 
 -- Move lines in insert mode
-vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
-vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("i", "<A-Down>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("i", "<A-Up>", "<Esc>:m .-2<CR>==gi")
 
 -- Move lines in visual mode
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
 
 -- Plugins
 vim.keymap.set("n", "<leader>pmd", ":MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
