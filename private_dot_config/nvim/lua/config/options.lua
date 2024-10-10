9745d0ce43cff5af9faac55eda02d69f445365a0
chris <94351434+gatekka@users.noreply.github.com>
2024-10-10 16:25:32 -0700

    Configure copy and paste text in Neovide

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -7,12 +7,17 @@ local opt = vim.opt
 opt.shiftwidth = 2 -- Size of an indent
 opt.relativenumber = false -- Relative line numbers
 
--- Neovide Begin
+-- Neovide Begin (More info at https://neovide.dev/configuration.html)
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   vim.g.neovide_input_macos_option_key_is_meta = "only_left"
   vim.g.neovide_cursor_vfx_mode = "railgun"
   vim.g.neovide_window_blurred = true
   vim.g.neovide_transparency = 0.9
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
 -- Neovide End
