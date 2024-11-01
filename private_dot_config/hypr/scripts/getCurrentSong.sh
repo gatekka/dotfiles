69a0ebaea793bd54473867ab2ac49eb2b27e15cc
gatekka <94351434+gatekka@users.noreply.github.com>
2024-11-01 13:41:22 -0700

    Display playerctl by checking if any sink-input isn't corked

--- private_dot_config/hypr/scripts/getCurrentSong.sh
+++ private_dot_config/hypr/scripts/getCurrentSong.sh
@@ -1,2 +1,9 @@
-song_info=$(playerctl metadata artist --format "{{artist}} - {{title}}")
-echo "$song_info"
+checkIfCorked() {
+  # Check if any sink-input is corked
+  if pactl list sink-inputs | grep -q 'Corked: no'; then
+    song_info=$(playerctl metadata artist --format "{{artist}} - {{title}}")
+    echo "$song_info"
+  fi
+}
+
+checkIfCorked
