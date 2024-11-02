aecf84d02de4190894754d7c375bd696b11eb9a8
gatekka <94351434+gatekka@users.noreply.github.com>
2024-11-01 18:15:24 -0700

    Optimize Cava script and improve performance

--- private_dot_config/waybar/scripts/cava.sh
+++ private_dot_config/waybar/scripts/cava.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Kill any existing cava processes at the start
-killall cava 2>/dev/null
-
-start_cava() {
-  is_cava_ServerExist=$(pgrep -x cava | wc -l)
-  if [ "$is_cava_ServerExist" -eq 0 ]; then
-    #Starting cava
+startCava() {
+  cavaProcessesAmount=$(pgrep -x cava | wc -l)
+  if [ "$cavaProcessesAmount" -eq 0 ]; then
     cava -p ~/.config/cava/config1 | sed -u 's/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;' &
   fi
 }
 
-# Kill cava if running
-stop_cava() {
-  killall cava
-  echo ""
+stopCava() {
+  pkill -9 cava && echo ""
   # cava_pid=$(pgrep -x cava)
   # if [ -n "$cava_pid" ]; then
   #   kill "$cava_pid"
@@ -24,31 +18,39 @@ stop_cava() {
 checkIfCorked() {
   # Check if any sink-input is corked
   if pactl list sink-inputs | grep -q 'Corked: no'; then
-    start_cava
+    startCava
   else
-    stop_cava
+    stopCava
   fi
 }
 
 isSpotifyOpen() {
-  # Check if Spotify is running
+  # Check if Spotify is open
   if pgrep -x "spotify" >/dev/null; then
-    #Spotify is running
-    start_cava
+    startCava
   else
-    #Spotify is not running. Stopping cava if active"
-    stop_cava
+    stopCava
   fi
 }
 
-while true; do
-  checkIfCorked
-done
+main() {
+  pkill -9 cava 2>/dev/null # Kill any existing cava processes at the start
+  while true; do
+    while pactl list sink-inputs | grep -q 'Corked: no'; do
+      startCava
+      sleep 1
+    done
+    stopCava
+    sleep 1
+  done
+}
+
+main
 
 # is_cava_ServerExist=$(ps -ef | grep -m 1 cava | grep -v "grep" | wc -l)
 # if [ "$is_cava_ServerExist" = "0" ]; then
 #   echo "cava_server not found" >/dev/null 2>&1
-# #	exit;
+#   exit
 # elif [ "$is_cava_ServerExist" = "1" ]; then
 #   killall cava
 # fi
