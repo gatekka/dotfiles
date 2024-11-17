9f9053970e824017cdd8f203b03fa95801482ba4
gatekka <94351434+gatekka@users.noreply.github.com>
2024-11-17 15:33:39 -0800

    Comment unused code

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- vim.g.root_spec = { "cwd" } -- Change root detection
+
 local opt = vim.opt
 
 opt.shiftwidth = 2 -- Size of an indent
@@ -14,10 +16,10 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_mode = "railgun"
   vim.g.neovide_window_blurred = true
   vim.g.neovide_transparency = 0.9
-  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  -- vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  -- vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  -- vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  -- vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  -- vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 end
 -- Neovide End
