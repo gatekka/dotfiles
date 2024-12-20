9b2fe2d488a775159755d7a32cf8a8647ea0ffee
chris <94351434+gatekka@users.noreply.github.com>
2024-12-19 19:22:49 -0800

    feat(sketchybar): completely restyle sketchybar, add spotify plugin, weather plugin, colored battery status, and different font

--- private_dot_config/sketchybar/plugins/executable_aerospace.sh
+++ private_dot_config/sketchybar/plugins/executable_aerospace.sh
@@ -4,7 +4,7 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-  sketchybar --set $NAME background.drawing=on
+  sketchybar --set "$NAME" background.drawing=on background.color=0x44ffffff
 else
-  sketchybar --set $NAME background.drawing=off
+  sketchybar --set "$NAME" background.drawing=off background.color=0x66494d64
 fi
