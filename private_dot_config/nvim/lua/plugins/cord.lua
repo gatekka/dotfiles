113fd4eafbb00d352ec59396d5d1f53355df9b64
gatekka <94351434+gatekka@users.noreply.github.com>
2025-01-06 19:40:03 -0800

    feat(nvim): enable cord.nvim plugin

--- private_dot_config/nvim/lua/plugins/cord.lua
+++ private_dot_config/nvim/lua/plugins/cord.lua
@@ -1,29 +1,29 @@
 -- https://github.com/vyfor/cord.nvim
 return {
-  -- {
-  --   "vyfor/cord.nvim",
-  --   build = "./build || .\\build",
-  --   event = "VeryLazy",
-  --   opts = {
-  --     editor = {
-  --       tooltip = "Neovim",
-  --     },
-  --     display = {
-  --       show_repository = true,
-  --       show_cursor_position = true,
-  --     },
-  --     lsp = {
-  --       show_problem_count = true,
-  --     },
-  --     text = {
-  --       viewing = "Viewing file",
-  --       editing = "Editing file",
-  --       file_browser = "Browsing files in Neotree",
-  --       plugin_manager = "Managing plugins",
-  --       lsp_manager = "Configuring LSP",
-  --       vcs = "Commiting changes",
-  --       workspace = "In workspace",
-  --     },
-  --   }, -- calls require('cord').setup()
-  -- },
+  {
+    "vyfor/cord.nvim",
+    build = "./build || .\\build",
+    event = "VeryLazy",
+    opts = {
+      editor = {
+        tooltip = "Neovim",
+      },
+      display = {
+        show_repository = true,
+        show_cursor_position = true,
+      },
+      lsp = {
+        show_problem_count = true,
+      },
+      text = {
+        viewing = "Viewing file",
+        editing = "Editing file",
+        file_browser = "Browsing files in Neotree",
+        plugin_manager = "Managing plugins",
+        lsp_manager = "Configuring LSP",
+        vcs = "Commiting changes",
+        workspace = "In workspace",
+      },
+    }, -- calls require('cord').setup()
+  },
 }
