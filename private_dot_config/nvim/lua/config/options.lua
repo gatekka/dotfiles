e6940c1132079303c7169472b7f1fe20d97bc406
gatekka <94351434+gatekka@users.noreply.github.com>
2025-04-30 16:22:04 -0700

    feat(hyprland): run syncthing on start up

--- private_dot_config/nvim/lua/config/options.lua
+++ private_dot_config/nvim/lua/config/options.lua
@@ -7,6 +7,7 @@
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
+vim.g.snacks_animate = false
 
 local opt = vim.opt
 
