49c98a213f185bd3fefafe470315416fd3273029
gatekka <94351434+gatekka@users.noreply.github.com>
2025-05-16 03:40:52 -0700

    feat(pywal): update script

--- private_dot_config/hypr/scripts/executable_pywal_update_apps.sh
+++ private_dot_config/hypr/scripts/executable_pywal_update_apps.sh
@@ -1,5 +1,18 @@
 main() {
+  reload-swaync-client
+  reload-waybar
+}
+
+reload-swaync-client() {
   swaync-client -rs # Reload css file for swaync
+}
+
+reload-waybar() {
+  if pgrep -x "waybar" >/dev/null; then
+    pkill -x "waybar"
+    sleep 1
+  fi
+  waybar &
 }
 
 main
