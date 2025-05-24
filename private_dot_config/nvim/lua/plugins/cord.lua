c52a4c92c46839a22225be4cd1aad3e522b0303b
gatekka <94351434+gatekka@users.noreply.github.com>
2025-05-24 04:10:05 -0700

    feat(nvim): Update `cord.nvim` plugin configuration for V2

--- private_dot_config/nvim/lua/plugins/cord.lua
+++ private_dot_config/nvim/lua/plugins/cord.lua
@@ -2,28 +2,63 @@
 return {
   {
     "vyfor/cord.nvim",
-    build = "./build || .\\build",
-    event = "VeryLazy",
+    build = ":Cord update",
     opts = {
+      variables = {
+        --   filename = "a file",
+        problems = function()
+          return #vim.diagnostic.get(0)
+        end,
+      },
       editor = {
         tooltip = "Neovim",
       },
-      display = {
-        show_repository = true,
-        show_cursor_position = true,
-      },
-      lsp = {
-        show_problem_count = true,
-      },
       text = {
-        viewing = "Viewing file",
-        editing = "Editing file",
-        file_browser = "Browsing files in Neotree",
-        plugin_manager = "Managing plugins",
-        lsp_manager = "Configuring LSP",
-        vcs = "Commiting changes",
-        workspace = "In workspace",
+        default = nil,
+        workspace = "In workspace - ${cursor_line}:${cursor_char}",
+        viewing = "Viewing ${filetype} file",
+        editing = "Editing ${filetype} file - ${problems} problems",
+        file_browser = "Browsing files in ${name}",
+        plugin_manager = "Managing plugins in ${name}",
+        lsp = "Configuring LSP in ${name}",
+        docs = "Reading ${name}",
+        vcs = "Committing changes in ${name}",
+        notes = "Taking notes in ${name}",
+        debug = "Debugging in ${name}",
+        test = "Testing in ${name}",
+        diagnostics = "Fixing problems in ${name}",
+        games = "Playing ${name}",
+        dashboard = "Home",
       },
-    }, -- calls require('cord').setup()
+      -- variables = true,
+    },
   },
 }
+-- return {
+--   {
+--     "vyfor/cord.nvim",
+--     build = "./build || .\\build",
+--     event = "VeryLazy",
+--     opts = {
+--       editor = {
+--         tooltip = "Neovim",
+--       },
+--       display = {
+--         show_repository = true,
+--         show_cursor_position = true,
+--       },
+--       lsp = {
+--         show_problem_count = true,
+--       },
+--       text = {
+--         viewing = "Viewing file",
+--         editing = "Editing file",
+--         file_browser = "Browsing files in Neotree",
+--         plugin_manager = "Managing plugins",
+--         lsp_manager = "Configuring LSP",
+--         vcs = "Commiting changes",
+--         workspace = "In workspace",
+--       },
+--     }, -- calls require('cord').setup()
+--   },
+-- }
