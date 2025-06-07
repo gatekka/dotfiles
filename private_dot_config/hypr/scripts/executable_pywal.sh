6fbc1c10ee1bc85ae8d46b40eba98d0934244f88
gatekka <94351434+gatekka@users.noreply.github.com>
2025-06-07 14:41:07 -0700

    feat(hypr) migrate from hyprwall to waypaper

---
+++ private_dot_config/hypr/scripts/executable_pywal.sh
@@ -0,0 +1,21 @@
+# Requires waypaper
+
+main() {
+    while true; do
+        # config_file="$HOME/.config/hyprwall/config.ini"
+        config_file="$HOME/.config/waypaper/config.ini"
+
+        # Wait for a valid file
+        while true; do
+            wallpaperPath="$HOME/$(sed -n 's/^wallpaper *= ~\/*//p' "$config_file")"
+            [ -f "$wallpaperPath" ] && break # Exit loop once file is valid
+            sleep 0.5
+        done
+
+        wal -i "$wallpaperPath" -n
+        bash "$HOME/.config/hypr/scripts/pywal_update_apps.sh" # Update pywal color scheme in external apps
+        inotifywait -e modify "$config_file"
+    done
+}
+
+main
