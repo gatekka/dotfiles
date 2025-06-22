2d997958caf006e2f53a5a7bab95c16b79d79470
gatekka <94351434+gatekka@users.noreply.github.com>
2025-06-21 21:39:46 -0700

    fix(hyprlock): fix hyprlock wallpaper not updating

--- private_dot_config/hypr/scripts/wallpaperForHyprlock.sh
+++ private_dot_config/hypr/scripts/wallpaperForHyprlock.sh
@@ -1,11 +1,20 @@
-# Whenever a user selects a new wallpaper with hyprwall this script will copy the current wallpaper to "$HOME/.config/hyprwall/wallpaper.jpg" which hyprwall can reference.
+# Whenever a user selects a new wallpaper with waypaper this script will copy the current wallpaper to "$HOME/.config/hyprwall/wallpaper.jpg" which hyprwall can reference.
+# Requires waypaper
+
 main() {
-  while true; do
-    config_file="$HOME/.config/hyprwall/config.ini"
-    wallpaperPath="$HOME/$(grep -oP ".wallpapers/[^\"]+" "$config_file")"
-    cp "$wallpaperPath" "$HOME/.config/hyprwall/wallpaper.jpg"
-    inotifywait -e modify "$config_file"
-  done
+    while true; do
+        config_file="$HOME/.config/waypaper/config.ini"
+
+        # Wait for a valid file
+        while true; do
+            wallpaperPath="$HOME/$(sed -n 's/^wallpaper *= ~\/*//p' "$config_file")"
+            [ -f "$wallpaperPath" ] && break # Exit loop once file is valid
+            sleep 0.5
+        done
+
+        cp "$wallpaperPath" "$HOME/.config/hyprwall/wallpaper.jpg"
+        inotifywait -e modify "$config_file"
+    done
 }
 
 main
