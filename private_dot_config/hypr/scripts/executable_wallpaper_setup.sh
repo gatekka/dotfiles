54f7e0a436959c2eb3ae3bcbf1ecdda4257b13a7
gatekka <94351434+gatekka@users.noreply.github.com>
2026-02-04 23:33:12 -0800

    refactor(hypr): simplify wallpaper setup and enable autostart script

--- private_dot_config/hypr/scripts/executable_wallpaper_setup.sh
+++ private_dot_config/hypr/scripts/executable_wallpaper_setup.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
-create_matugen_templates() {
-  while true; do
-    config_file="$HOME/.config/waypaper/config.ini"
-    inotifywait -e modify "$config_file"
-
-    # Wait for a valid file
-    while true; do
-      wallpaperPath="$HOME/$(sed -n 's/^wallpaper *= ~\/*//p' "$config_file")"
-      [ -f "$wallpaperPath" ] && break # Exit loop once file is valid
-      sleep 1
-    done
-
-    matugen image "$wallpaperPath"
-  done
-}
+# create_matugen_templates() {
+#   while true; do
+#     config_file="$HOME/.config/waypaper/config.ini"
+#     inotifywait -e modify "$config_file"
+#
+#     # Wait for a valid file
+#     while true; do
+#       wallpaperPath="$HOME/$(sed -n 's/^wallpaper *= ~\/*//p' "$config_file")"
+#       [ -f "$wallpaperPath" ] && break # Exit loop once file is valid
+#       sleep 1
+#     done
+#
+#     matugen image "$wallpaperPath"
+#   done
+# }
 
 main() {
-  waypaper --restore         # restore wallpaper
-  swww-daemon                # start wallpaper daemon
-  create_matugen_templates & # generate matugen templates
+  config_file="$HOME/.config/waypaper/config.ini"
+  wallpaperPath="$HOME/$(sed -n 's/^wallpaper *= ~\/*//p' "$config_file")"
+  # waypaper --restore # restore wallpaper
+  swww-daemon # start wallpaper daemon
+  matugen image "$wallpaperPath"
+  # create_matugen_templates & # generate matugen templates
 }
 
 main "$@" # "$@" passes all positional parameters to script if needed
